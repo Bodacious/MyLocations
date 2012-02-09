435fc4eec70500cfae7bafb8ed97c7eb8e3eeb07
Bodacious <gavin@katanacode.com>
2012-02-09 21:03:35 +0000

    Pushing to github - will try and build from iMac instead

--- MyLocations/CurrentLocationViewController.m
+++ MyLocations/CurrentLocationViewController.m
@@ -1,12 +1,5 @@
-//
-//  CurrentLocationViewController.m
-//  MyLocations
-//
-//  Created by Gavin Morrice on 08/02/2012.
-//  Copyright (c) 2012 Katana Code Ltd. All rights reserved.
-//
-
 #import "CurrentLocationViewController.h"
+#import "LocationDetailsViewController.h"
 
 // Forward declaration
 @interface CurrentLocationViewController()
---
+++ MyLocations/LocationDetailsViewController.h
@@ -0,0 +1,13 @@
+@interface LocationDetailsViewController : UITableViewController
+
+@property (nonatomic, strong) IBOutlet UITextView *descriptionTextView;
+@property (nonatomic, strong) IBOutlet UILabel *categoryLabel;
+@property (nonatomic, strong) IBOutlet UILabel *latitudeLabel;
+@property (nonatomic, strong) IBOutlet UILabel *longitudeLabel;
+@property (nonatomic, strong) IBOutlet UILabel *addressLabel;
+@property (nonatomic, strong) IBOutlet UILabel *dateLabel;
+
+- (IBAction)done:(id)sender;
+- (IBAction)cancel:(id)sender;
+
+@end
